bb8a1ddd06d8016a0d494b70a11b984614207efa
Flarch <beni.vizer@gmail.com>
2024-11-28 16:46:59 +0100

    Remove __del__

--- depth_mesh_pro.py
+++ depth_mesh_pro.py
@@ -312,10 +312,6 @@ class DepthPredict(bpy.types.Operator):
         if self.focal_length is not None:
             del self.focal_length
             self.focal_length = None
-
-    def __del__(self):
-        # Ensure cleanup is called when the operator is deleted
-        self.cleanup()
         
 
 #print(f"depth_mesh_gen time: {time.time()-start}")

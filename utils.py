e4e8ae913a4a4be44402b6c2a1a9a55c6b9cc98e
Flarch <beni.vizer@gmail.com>
2024-12-01 14:43:37 +0100

    Closes #18: Fixed GPU performance request from table by name

--- utils.py
+++ utils.py
@@ -19,20 +19,22 @@ def get_cpu_mflops():
 def get_gpu_mflops():
     import pandas as pd
     import os
-    gpu_name_full = gpu.platform.renderer_get()
-    vendor = gpu_name_full.split(" ")[0] # example: NVIDIA
-    gpu_name = gpu_name_full.split("/")[0].split(" ")[1:] # example: GeForce RTX 3060 Ti
+    gpu_name_full = gpu.platform.renderer_get() # e.g. str:NVIDIA GeForce RTX 3060 Ti/PCIe/SSE2
+    vendor = gpu_name_full.split(" ")[0] # e.g. str:NVIDIA
+    gpu_name = gpu_name_full.split("/")[0].split(" ")[1:] # e.g. List:[GeForce,RTX,3060,Ti]
     
-    delim = " "
-    gpu_name = delim.join(gpu_name)
+    delim = ""
+    gpu_name = delim.join(gpu_name) # e.g. GeForceRTX3060Ti
     
     file_dir = os.path.dirname(__file__)
     file_name = vendors[vendor]
     
     df = pd.read_csv(os.path.join(file_dir,file_name))
     
-    # Filter rows by the value of their first column
-    filtered_df = df[df.iloc[:, 0] == gpu_name]
+    # Filter rows by the value of their first column also convert to lower case and remove spaces
+    filtered_df = df[df.apply(lambda x: x.astype(str).str.lower().str.replace(" ","") == gpu_name.lower()).any(axis=1)]
+    print(filtered_df)
+    #filtered_df = df[df.iloc[:, 0] == gpu_name]
     if not filtered_df.empty:
         return int(filtered_df.iloc[0, 9])
     else:
